843a2b62b49cfe0b96bc14cfa6ba76791673ead1
HolyPi <joanelly2018@gmail.com>
2021-02-18 16:16:51 -0500

    Added more info + completed challenges

---
+++ index.js
@@ -0,0 +1,43 @@
+function getWeatherByZip(apikey, zip, units = 'imperial') {
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
+  return getWeather(path)
+}
+
+function getWeatherByCity(apikey, city, units = 'imperial') {
+  const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`
+  return getWeather(path)
+}
+
+function getWeatherByGeo(apikey, lat, lon, units = 'imperial') {
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
+  return getWeather(path)
+}
+
+async function getWeather(path) {
+
+    try {
+      const res = await fetch(path)
+      const json = await res.json()
+      console.log(json)
+      const weatherData = {
+        code: json.cod,
+        temp: json.main.temp,
+        description: json.weather[0].description,
+        name: json.name,
+        feels_like: json.main.feels_like,
+        temp_min: json.main.temp_min,
+        temp_max: json.main.temp_max
+      }
+      return weatherData 
+
+    } catch(err) {
+
+      return err
+    } 
+  }
+
+export {
+  getWeatherByZip,
+  getWeatherByCity,
+  getWeatherByGeo
+}
